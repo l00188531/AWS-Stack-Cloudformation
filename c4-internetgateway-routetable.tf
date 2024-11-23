9a8fda93b97c012790f6d62b72244a3e89b81d00
l00188531 <l00188531@atu.ie>
2024-11-23 21:30:18 +0000

    terraform secript validated

--- c4-internetgateway-routetable.tf
+++ c4-internetgateway-routetable.tf
@@ -1,16 +1,16 @@
 # Create Internet Gateway
-resource "aws_internet_gateway" "my3tier_igw" {
-  vpc_id = aws_vpc.my3tier_vpc.id
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
 
   tags = {
-    Name = "My3Tier-IGW"
+    Name = "My-IGW"
   }
 }
 
 
 # Create route table
 resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.my3tier_vpc.id
+  vpc_id = aws_vpc.my_vpc.id
 
   tags = {
     Name = "Public-Route-Table"
@@ -21,11 +21,11 @@ resource "aws_route_table" "public_route_table" {
 resource "aws_route" "public_route" {
   route_table_id         = aws_route_table.public_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.my3tier_igw.id
+  gateway_id             = aws_internet_gateway.my_igw.id
 }
 
 # Associate route table with public subnet
 resource "aws_route_table_association" "public_route_association" {
-  subnet_id      = aws_subnet.public_subnet_fe.id
+  subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_route_table.id
 }
